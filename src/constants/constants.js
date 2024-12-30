da7228db6a911a5832b160e21e7dd6010874429c
manju <bargavemanju@gmail.com>
2024-12-30 21:58:15 +0530

    "Changing url in constants .js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,4 @@
 
-export const API_BASE_URL='https://manjubargave0202.pythonanywhere.com/'
+export const API_BASE_URL='https://manjubargave0202.pythonanywhere.com'
 
 export const PATHS={MASTERS:'/masters',NOTES:'/notes',}
