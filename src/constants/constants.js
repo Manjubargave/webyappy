caaa6c8cea926a12950909dbbc0cd89eed331ff1
manju <bargavemanju@gmail.com>
2025-01-02 17:19:53 +0530

    Changing backend url to the local host

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,6 @@
 
-export const API_BASE_URL='https://manjubargave0202.pythonanywhere.com'
+export const API_BASE_URL='http://127.0.0.1:8000'
+
+//'https://manjubargave0202.pythonanywhere.com'
 
 export const PATHS={MASTERS:'/masters',NOTES:'/notes',}
